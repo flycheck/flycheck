a151210985c0d1fe7d0b960d1031ef0c3e3ddf93
Sebastian Wiesner <lunaryorn@gmail.com>
2012-10-10 16:16:29 +0200

    Add initial skeleton

---
+++ flymake-checkers.el
@@ -0,0 +1,40 @@
+;;; flymake-checkers.el --- Additional syntax checkers for flymake
+;;; -*- coding: utf-8; lexical-binding: t -*-
+
+;; Copyright (c) 2012 Sebastian Wiesner <lunaryorn@gmail.com>
+;;
+;; Author: Sebastian Wiesner <lunaryorn@gmail.com>
+;; URL: https://github.com/lunaryorn/flymake-checkers
+;; Version: 0.1
+;; Keywords: convenience languages tools
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free Software
+;; Foundation; either version 2 of the License, or (at your option) any later
+;; version.
+
+;; This program is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+;; FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+;; details.
+
+;; You should have received a copy of the GNU General Public License along with
+;; this program; if not, write to the Free Software Foundation, Inc., 51
+;; Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+
+;; Additional checkers for `flymake-mode'.
+
+;; Provide checkers for:
+;;
+;; - `sh-mode`
+
+;;; Code:
+
+
+(provide 'flymake-checkers)
+
+;;; flymake-checkers.el ends here
