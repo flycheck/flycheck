e958dc9541a25e5de846827fe95caa7438c7461a
Sebastian Wiesner <lunaryorn@gmail.com>
2012-10-10 16:16:44 +0200

    Add support for sh-mode

--- flymake-checkers.el
+++ flymake-checkers.el
@@ -34,6 +34,64 @@
 
 ;;; Code:
 
+(eval-when-compile
+  (require 'flymake))
+
+;; Utility functions
+
+(defun flymake-checkers-create-temp-system (filename prefix)
+  "Create a copy of FILENAME with PREFIX in temp directory.
+
+Return the path of the file."
+  (make-temp-file (or prefix "flymake-checkers") nil
+                  (concat "." (file-name-extension filename))))
+
+
+;; sh-mode
+
+(defconst flymake-checkers-sh-supported-shells '(bash zsh sh)
+  "Shells supported by `flymake-checkers-sh-init'.")
+
+;;;###autoload
+(defun flymake-checkers-sh-init ()
+  "Initialize flymake checking for `sh-mode'."
+  (if (memq sh-shell flymake-checkers-sh-supported-shells)
+      `(,(symbol-name sh-shell) ("-n" ,(flymake-init-create-temp-buffer-copy
+                                         'flymake-checkers-create-temp-system)))
+    (flymake-log 1 "Shell %s is not supported by flymake." sh-shell)
+    nil))
+
+
+;; Register checkers in flymake
+
+;;;###autoload
+(defconst flymake-checkers-file-name-masks
+  '(("\\.sh\\'" flymake-checkers-sh-init)
+    ("\\.zsh\\'" flymake-checkers-sh-init)
+    ("\\.bash\\'" flymake-checkers-sh-init))
+  "All checkers provided by flymake-checkers with corresponding.
+
+Automatically added to `flymake-allowed-file-name-masks' when
+this package is loaded.")
+
+;;;###autoload
+(defconst flymake-checkers-err-line-patterns
+  '(("^\\(.+\\): line \\([0-9]+\\): \\(.+\\)$" 1 2 nil 3) ; bash
+    ("^\\(.+\\): ?\\([0-9]+\\): \\(.+\\)$" 1 2 nil 3))
+  "Additional error line patterns.
+
+Automatically added to `flymake-err-line-patterns' when this
+package is loaded.")
+
+;;;###autoload
+(eval-after-load 'flymake
+  #'(mapc (lambda (cell) (add-to-list 'flymake-allowed-file-name-masks cell))
+          flymake-checkers-file-name-masks))
+
+;;;###autoload
+(eval-after-load 'flymake
+  #'(setq flymake-err-line-patterns (append flymake-checkers-err-line-patterns
+                                            flymake-err-line-patterns)))
 
 (provide 'flymake-checkers)
 
