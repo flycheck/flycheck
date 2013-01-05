e8596f4497b0fbe6fbed257b8ad1ea038a872da7
Sebastian Wiesner <lunaryorn@gmail.com>
2013-01-05 17:07:35 +0100

    Recursively load unit test files

--- tests/testsuite.el
+++ tests/testsuite.el
@@ -41,7 +41,7 @@
 ;; Load flycheck
 (load (expand-file-name "../flycheck.el" testsuite-dir) nil :no-message)
 
-(defconst testsuite-test-file-pattern "^.*/test-.+$"
+(defconst testsuite-test-file-pattern "^.*/test-[^/]+$"
   "Regular expression to match names of test files.")
 
 (defun testsuite-test-file-p (filename)
@@ -49,18 +49,30 @@
   (and (s-matches? testsuite-test-file-pattern filename)
        (file-regular-p filename)))
 
-(defun testsuite-find-all-test-files (&optional directory)
-  "Find and return all test files in DIRECTORY.
+(defun testsuite-test-dir-p (dirname)
+  "Determine whether DIRNAME is a test directory."
+  (and (s-matches? testsuite-test-file-pattern dirname)
+       (file-directory-p dirname)))
 
-If omitted DIRECTORY is `testsuite-dir'."
-  (let* ((directory (or directory testsuite-dir))
-         (contents (directory-files directory :full-names)))
-     (-filter 'testsuite-test-file-p contents)))
+(defun testsuite-find-all-test-files (directory)
+  "Recursively all test files in DIRECTORY."
+  (let* ((contents (directory-files directory :full-names))
+         (test-files (-filter 'testsuite-test-file-p contents))
+         (test-dirs (-filter 'testsuite-test-dir-p contents)))
+    (append test-files
+            (-flatten (-map 'testsuite-find-all-test-files test-dirs))
+            nil)))
 
-(defconst testsuite-testfiles (testsuite-find-all-test-files)
+(defconst testsuite-testfiles (testsuite-find-all-test-files testsuite-dir)
   "All test files of this test suite.")
 
-(dolist (testfile testsuite-testfiles)
-  (load testfile nil :no-message))
+(message "Test files: %s" testsuite-testfiles)
+
+(defun testsuite-load-testfile (testfile)
+  "Load a TESTFILE."
+  (let ((default-directory (file-name-directory testfile)))
+    (load testfile nil :no-message)))
+
+(-each testsuite-testfiles 'testsuite-load-testfile)
 
 ;;; testsuite.el ends here
