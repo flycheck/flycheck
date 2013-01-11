00c6a91cbc397d87e32babf6c447667488d37a2e
Sebastian Wiesner <lunaryorn@gmail.com>
2013-01-11 13:28:26 +0100

    Add unit tests for sh checker

---
+++ tests/test-sh/missing-quote.sh
@@ -0,0 +1,5 @@
+# A syntax error resulting from a missing quote
+
+echo 'foo
+
+echo "Hello World"
---
+++ tests/test-sh/test-sh.el
@@ -0,0 +1,48 @@
+;;; test-sh.el --- Test the sh checker
+
+;; Copyright (c) 2013 Sebastian Wiesner <lunaryorn@gmail.com>
+;;
+;; Author: Sebastian Wiesner <lunaryorn@gmail.com>
+;; URL: https://github.com/lunaryorn/flycheck
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Code:
+
+(require 'ert)
+(require 'flycheck)
+
+(require 'sh-script)
+
+(testsuite-module "sh")
+
+(defun shsh-mode ()
+  (sh-mode)
+  (sh-set-shell "sh" :no-query))
+
+(ert-deftest sh-missing-quote ()
+  "Test a syntax error from a missing quote."
+  (should-flycheck-checker
+   (testsuite-sh-resource "missing-quote.sh") 'shsh-mode 'sh
+   '(6 nil "Syntax error: Unterminated quoted string" error)))
+
+(ert-deftest sh-missing-semicolon ()
+  "Test a syntax error from a missing semicolon."
+  (should-flycheck-checker
+   (testsuite-sh-resource "missing-semicolon.sh") 'shsh-mode 'sh
+   '(5 nil "Syntax error: \"fi\" unexpected (expecting \"then\")" error)))
+
+;;; test-sh.el ends here
