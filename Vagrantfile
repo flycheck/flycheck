5cb0f0903119b5dea9d083b0c5bbb238f2848bcd
Sebastian Wiesner <lunaryorn@gmail.com>
2013-01-23 13:11:46 +0100

    Go back to Shell script for provision
    Shell scripts have a more compact syntax.  Also try to install packages
    with the same package manager at once, to avoid superfluous process
    executions and dependency calculations.

--- Vagrantfile
+++ Vagrantfile
@@ -5,5 +5,5 @@ Vagrant::Config.run do |config|
   config.vm.box = "precise64"
   config.vm.box_url = "http://files.vagrantup.com/precise64.box"
 
-  config.vm.provision :shell, :path => "vagrant/provision.py"
+  config.vm.provision :shell, :path => "vagrant/provision.sh"
 end
---
+++ vagrant/provision.sh
@@ -0,0 +1,88 @@
+#!/bin/sh
+# Copyright (C) 2013  Sebastian Wiesner <lunaryorn@gmail.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+# Provision a VM with necessary packages to test all Flycheck checkers
+
+ppa () {
+    sudo apt-add-repository -y "$1"
+}
+
+apt_update () {
+    sudo apt-get update -qq
+}
+
+apt () {
+    sudo apt-get install -yy "$@"
+}
+
+npm () {
+    sudo npm install -g "$@"
+}
+
+pip () {
+    sudo pip install "$@"
+}
+
+gem () {
+    # Do not force-install Gems as root, because this breaks on Travis CI
+    command gem install "$@"
+}
+
+# Silence debconf
+export DEBIAN_FRONTEND='noninteractive'
+
+# Update repositories to prevent errors caused by missing packages
+apt_update
+
+# Install basic packages
+apt make python-software-properties
+
+# Bring in the necessary PPAs
+ppa ppa:cassou/emacs
+ppa ppa:ondrej/php5
+ppa ppa:richarvey/nodejs
+apt_update
+
+# Install Emacs 24.2 and Emacs snapshot
+apt emacs24 emacs24-el emacs24-common-non-dfsg \
+    emacs-snapshot emacs-snapshot-el
+
+# Install the necessary interpreters and tools
+apt nodejs npm \
+    python python-pip \
+    ruby1.9.1
+
+# Install all checker tools
+# HACK: Install Lua mode from repositories, because the MELPA package causes
+# tests to hang on Emacs snapshot
+apt bash \
+    tidy \
+    lua5.2 lua-mode \
+    php5-cli \
+    chktex lacheck \
+    xmlstarlet \
+    zsh
+
+pip flake8 pylint pyflakes
+
+npm coffee-script coffeelint \
+    csslint \
+    jshint \
+    jsonlint
+
+gem haml \
+    sass
