6c022dc2652f351d2d01c081cfc3b6e63f1bf68f
Sebastian Wiesner <lunaryorn@gmail.com>
2013-04-09 16:10:43 +0200

    [Fix #102] Shorten description
    Fix `M-x list-packages`

--- flycheck-pkg.el
+++ flycheck-pkg.el
@@ -1,12 +1,3 @@
 (define-package "flycheck" "0.8"
-  "Flymake done right
-
-Flycheck provides on-the-fly syntax checker with
-
-- major-mode based checkers,
-- simple declarative checker definitions,
-- built-in checkers for many languages,
-- standard error navigation,
-- easy customization,
-- and a clean, concise and understandable implementation."
+  "On-the-fly syntax checking (Flymake done right)"
   '((s "1.3.1") (dash "1.0.3")))
