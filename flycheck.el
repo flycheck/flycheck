flycheck/flycheck

commit d9f248a8f9b0d8eca771d1380cd3aeee30f8f3ae

Add missing customization group

flycheck.el
@@ -32,7 +32,7 @@
 ;; complete list).
 ;;
 ;; Support for new modes and languages can be added by declaring a new syntax
-;; checker (see `flycheck-declare-checker').
+1;; checker (see `flycheck-declare-checker').
 
 ;;; Code:
 
@@ -78,7 +78,12 @@ buffer-local wherever it is set."
   :group 'tools)
 
 (defgroup flycheck-config-files nil
-  "Configuration files for on-the-fly syntax checking."
+  "Configuration files for on-the-fly syntax checkers."
+  :prefix "flycheck-"
+  :group 'flycheck)
+
+(defgroup flycheck-options nil
+  "Options for on-the-fly syntax checkers."
   :prefix "flycheck-"
   :group 'flycheck)
 

doc/flycheck.info
@@ -1696,6 +1696,10 @@ master (unreleased)
         * The 'ruby' syntax checker now differentiates warnings from
           errors (https://github.com/lunaryorn/flycheck/issues/123)
 
+   * Bug fixes:
+
+        * Add missing customization group for syntax checker options
+
 
 File: flycheck.info,  Node: Credits,  Next: GNU Free Documentation License,  Prev: Changes,  Up: Top
 
@@ -2381,9 +2385,9 @@ Node: 0.859342
 Node: 0.960854
 Node: 0.1061350
 Node: master63404
-Node: Credits63668
-Node: GNU Free Documentation License66367
-Node: Syntax checkers91519
-Node: Function and Variable Index92495
+Node: Credits63755
+Node: GNU Free Documentation License66454
+Node: Syntax checkers91606
+Node: Function and Variable Index92582
 
 End Tag Table

commit 5f4bc28664d61eebe73a4a21d5dd07af97aad0b0

Delete garbage character
Regression from d9f248a8

flycheck.el
@@ -32,7 +32,7 @@
 ;; complete list).
 ;;
 ;; Support for new modes and languages can be added by declaring a new syntax
-1;; checker (see `flycheck-declare-checker').
+;; checker (see `flycheck-declare-checker').
 
 ;;; Code:
 