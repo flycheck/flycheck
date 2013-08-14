flycheck/flycheck

commit fcc62cf010a1839d2b5eac3a54292773a230d957

Don't install latest packages with npm
The NPM provider fails to handle latest packages which are already at
the latest version, see puppetlabs/puppetlabs-nodejs#43.

puppet/modules/flycheck/manifests/checkers/nodejs.pp
@@ -20,7 +20,7 @@ class flycheck::checkers::nodejs {
                     ]
 
   package { $node_packages:
-    ensure   => latest,
+    ensure   => ensure,
     provider => npm,
     require  => Package['nodejs'],
   }

commit d536ce6ffc6f1daab3931cd1f3cddf038dce894f

Fix npm package installation
Regression from fcc62cf.  Pretty damn stupid mistake :(

puppet/modules/flycheck/manifests/checkers/nodejs.pp
@@ -20,7 +20,7 @@ class flycheck::checkers::nodejs {
                     ]
 
   package { $node_packages:
-    ensure   => ensure,
+    ensure   => present,
     provider => npm,
     require  => Package['nodejs'],
   }