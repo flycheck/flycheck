ebf7e5c1c16803387e8781022a12a6630f56ca8c
Sebastian Wiesner <lunaryorn@gmail.com>
2013-08-15 12:29:44 +0200

    Bootstrap a decent version of Pip

--- puppet/modules/flycheck/manifests/checkers/python.pp
+++ puppet/modules/flycheck/manifests/checkers/python.pp
@@ -1,13 +1,37 @@
 # Syntax checkers for Python
 
 class flycheck::checkers::python {
-  package { ['python', 'python-pip']: ensure => latest }
+  package { ['python', 'python-setuptools'] : ensure => latest }
 
-  # Get the latest distribute and pip
+  # Bootstrap a decent Pip release
+  $pip = 'pip-1.4.1'
+  $pip_archive = "${pip}.tar.gz"
+
+  archive { $pip_archive:
+    ensure        => present,
+    url           => "https://pypi.python.org/packages/source/p/pip/${pip_archive}",
+    digest_string => '6afbb46aeb48abac658d4df742bff714',
+    target        => '/usr/src',
+  }
+
+  exec { 'flycheck::checkers::python::bootstrap-pip':
+    command => '/usr/bin/python setup.py install',
+    cwd     => "/usr/src/${pip}",
+    creates => '/usr/local/bin/pip',
+    require => [Package['python-setuptools'], Archive[$pip_archive]],
+  }
+
+  # Now upgrade to the latest version of setuptools and pip
   package { 'setuptools':
     ensure   => latest,
     provider => pip,
-    require  => Package['python-pip'],
+    require  => Exec['flycheck::checkers::python::bootstrap-pip'],
+  }
+
+  package { 'pip':
+    ensure   => latest,
+    provider => pip,
+    require  => Package['setuptools'],
   }
 
   $python_packages = ['flake8',      # python-flake8
@@ -19,6 +43,6 @@ class flycheck::checkers::python {
   package { $python_packages:
     ensure   => latest,
     provider => pip,
-    require  => [Package['setuptools'], Package['python-pip']],
+    require  => Package['pip'],
   }
 }
