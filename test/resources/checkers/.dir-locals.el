83144242df9f3f1dba9b465fcc8a2b16063095e7
Sebastian Wiesner <lunaryorn@gmail.com>
2013-09-14 14:41:40 +0200

    Configure indentation styles for C/C++ sources

---
+++ test/resources/checkers/.dir-locals.el
@@ -0,0 +1,8 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((c++-mode
+  (c-file-style . "stroustrup"))
+ (c-mode
+  (indent-tabs-mode . t)
+  (c-file-style . "linux")))
