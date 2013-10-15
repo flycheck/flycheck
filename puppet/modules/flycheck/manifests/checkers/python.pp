798affe75b660b7d4f696bf58d8cf9f311344f89
Sebastian Wiesner <lunaryorn@gmail.com>
2013-10-15 13:02:49 +0200

    Simplify Pip bootstrapping

--- puppet/modules/flycheck/manifests/checkers/python.pp
+++ puppet/modules/flycheck/manifests/checkers/python.pp
@@ -1,40 +1,23 @@
 # Syntax checkers for Python
 
 class flycheck::checkers::python {
-  package { ['python', 'python-setuptools'] : ensure => latest }
+  package { 'python': ensure => latest}
 
-  # Bootstrap a decent Pip release
-  $pip = 'pip-1.4.1'
-  $pip_archive = "${pip}.tar.gz"
-
-  archive { $pip_archive:
-    ensure        => present,
-    url           => "https://pypi.python.org/packages/source/p/pip/${pip_archive}",
-    digest_string => '6afbb46aeb48abac658d4df742bff714',
-    target        => '/usr/src',
+  package { 'python-setuptools' :
+    ensure  => latest,
+    notify  => Exec['flycheck::checkers::python::bootstrap-pip'],
+    require => Package['python'],
   }
 
   exec { 'flycheck::checkers::python::bootstrap-pip':
-    command => '/usr/bin/python setup.py install',
-    cwd     => "/usr/src/${pip}",
-    creates => '/usr/local/bin/pip',
-    require => [Package['python-setuptools'], Archive[$pip_archive]],
+    command     => '/usr/bin/easy_install -U setuptools pip',
+    refreshonly => true,
+    require     => Package['python-setuptools'],
   }
 
-  # Now upgrade to the latest version of setuptools and pip
-  package { 'setuptools':
-    ensure   => latest,
-    provider => pip,
-    require  => Exec['flycheck::checkers::python::bootstrap-pip'],
-  }
-
-  package { 'pip':
-    ensure   => latest,
-    provider => pip,
-    require  => Package['setuptools'],
-  }
-
-  $python_packages = ['flake8',      # python-flake8
+  $python_packages = ['setuptools',
+                      'pip',
+                      'flake8',      # python-flake8
                       'pep8-naming', # Plugin for flake8
                       'pylint',      # python-pylint
                       'docutils',    # rst
@@ -43,6 +26,6 @@ class flycheck::checkers::python {
   package { $python_packages:
     ensure   => latest,
     provider => pip,
-    require  => Package['pip'],
+    require  => Exec['flycheck::checkers::python::bootstrap-pip'],
   }
 }
