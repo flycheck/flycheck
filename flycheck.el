48c1a59b466d4515694ae46f3eb715c382759771
Yasuyuki Oka <yasuyk@gmail.com>
2013-10-26 12:21:14 +0900

    Add syntax checker for eRuby using erubis command

--- flycheck.el
+++ flycheck.el
@@ -132,6 +132,7 @@ buffer-local wherever it is set."
     emacs-lisp
     emacs-lisp-checkdoc
     erlang
+    eruby-erubis
     go-gofmt
     go-build
     go-test
@@ -3850,6 +3851,19 @@ The checker runs `checkdoc-current-buffer'."
   ((warning line-start (file-name) ":" line ": Warning:" (message) line-end)
    (error line-start (file-name) ":" line ": " (message) line-end))
   :modes erlang-mode)
+
+(flycheck-define-checker eruby-erubis
+  "A eRuby syntax checker using the `erubis' command.
+
+See URL `http://www.kuwata-lab.com/erubis/'."
+  :command ("erubis" "-z" source)
+  :error-patterns
+  ((error line-start  (file-name) ":" line ": " (message) line-end))
+  :predicate
+  (lambda ()
+    (or (memq major-mode '(html-erb-mode rhtml-mode))
+        (and buffer-file-name
+             (string= "erb" (file-name-extension buffer-file-name))))))
 
 (flycheck-define-checker go-gofmt
   "A Go syntax and style checker using the gofmt utility.
