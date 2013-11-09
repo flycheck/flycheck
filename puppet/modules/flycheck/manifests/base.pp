987fcf045f3b4ec6c309bc4e977e3e1ae8df306d
Sebastian Wiesner <lunaryorn@gmail.com>
2013-11-09 14:41:43 +0100

    Configure Apt sources to use Ubuntu mirrors
    Makes local package downloads much faster

--- puppet/modules/flycheck/manifests/base.pp
+++ puppet/modules/flycheck/manifests/base.pp
@@ -1,12 +1,50 @@
 # Basic system setup
 
 class flycheck::base {
-  include apt
+  class { 'apt':
+    # When not on Travis, construct our own sources.list to pick good mirrors
+    purge_sources_list => true,
+  }
+
+  # Use Ubuntu mirrors to get fast package downloads
+  apt::source { 'ubuntu':
+    location => 'mirror://mirrors.ubuntu.com/mirrors.txt',
+    release  => 'precise',
+    repos    => 'main restricted universe multiverse',
+  }
+
+  apt::source { 'ubuntu-security':
+    location => 'http://security.ubuntu.com/ubuntu',
+    release  => 'precise-security',
+    repos    => 'main restricted universe multiverse',
+  }
+
+  apt::source { 'ubuntu-updates':
+    location => 'mirror://mirrors.ubuntu.com/mirrors.txt',
+    release  => 'precise-updates',
+    repos    => 'main restricted universe multiverse',
+  }
+
+  apt::source { 'ubuntu-backports':
+    location => 'mirror://mirrors.ubuntu.com/mirrors.txt',
+    release  => 'precise-backports',
+    repos    => 'main restricted universe multiverse',
+  }
+
+  # Required to add PPAs.  Install first, before re-configuring Apt, to avoid a
+  # dependency circle around apt::update
+  package { 'python-software-properties':
+    ensure  => latest,
+    require => [],
+    before  => Class['apt'],
+  }
+
+  # Update Apt before installing packages
+  Package {
+    require => Class['apt::update']
+  }
 
   package { 'make': ensure => latest }
-
-  # Required to add PPAs
-  package { 'python-software-properties': ensure => latest }
 
   # Archive tools to extract Carton and syntax checker archives
   package { ['tar', 'unzip']: ensure => latest }
