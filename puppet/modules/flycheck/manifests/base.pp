1c6a47c3c8da6696e27523b611f96bb4cf36eda4
Sebastian Wiesner <lunaryorn@gmail.com>
2013-11-11 12:14:35 +0100

    Don't change sources.list when provisioning Travis

--- puppet/modules/flycheck/manifests/base.pp
+++ puppet/modules/flycheck/manifests/base.pp
@@ -1,34 +1,42 @@
 # Basic system setup
 
 class flycheck::base {
+
+  $should_use_own_sources_list = $::travis ? {
+    undef   => false,
+    default => true,
+  }
+
   class { 'apt':
     # When not on Travis, construct our own sources.list to pick good mirrors
-    purge_sources_list => true,
+    purge_sources_list => $should_use_own_sources_list,
   }
 
-  # Use Ubuntu mirrors to get fast package downloads
-  apt::source { 'ubuntu':
-    location => 'mirror://mirrors.ubuntu.com/mirrors.txt',
-    release  => 'precise',
-    repos    => 'main restricted universe multiverse',
-  }
+  if $should_use_own_sources_list {
+    # Use Ubuntu mirrors to get fast package downloads
+    apt::source { 'ubuntu':
+      location => 'mirror://mirrors.ubuntu.com/mirrors.txt',
+      release  => 'precise',
+      repos    => 'main restricted universe multiverse',
+    }
 
-  apt::source { 'ubuntu-security':
-    location => 'http://security.ubuntu.com/ubuntu',
-    release  => 'precise-security',
-    repos    => 'main restricted universe multiverse',
-  }
+    apt::source { 'ubuntu-security':
+      location => 'http://security.ubuntu.com/ubuntu',
+      release  => 'precise-security',
+      repos    => 'main restricted universe multiverse',
+    }
 
-  apt::source { 'ubuntu-updates':
-    location => 'mirror://mirrors.ubuntu.com/mirrors.txt',
-    release  => 'precise-updates',
-    repos    => 'main restricted universe multiverse',
-  }
+    apt::source { 'ubuntu-updates':
+      location => 'mirror://mirrors.ubuntu.com/mirrors.txt',
+      release  => 'precise-updates',
+      repos    => 'main restricted universe multiverse',
+    }
 
-  apt::source { 'ubuntu-backports':
-    location => 'mirror://mirrors.ubuntu.com/mirrors.txt',
-    release  => 'precise-backports',
-    repos    => 'main restricted universe multiverse',
+    apt::source { 'ubuntu-backports':
+      location => 'mirror://mirrors.ubuntu.com/mirrors.txt',
+      release  => 'precise-backports',
+      repos    => 'main restricted universe multiverse',
+    }
   }
 
   # Required to add PPAs.  Install first, before re-configuring Apt, to avoid a
