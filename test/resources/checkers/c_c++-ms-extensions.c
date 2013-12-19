27751eaf04c6f94dff37cc3800b18ff035bfdf06
Mitch Tishmack <Mitchell.J.Tishmack@usps.gov>
2013-12-19 11:21:44 -0600

    squashme: knock down the test case a bit.

--- test/resources/checkers/c_c++-ms-extensions.c
+++ test/resources/checkers/c_c++-ms-extensions.c
@@ -1,10 +1,5 @@
-#include <stdio.h>
-
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wmicrosoft"
-
 typedef struct {
-  double a;
+  int a;
 } inner_s;
 
 typedef struct {
@@ -12,19 +7,10 @@ typedef struct {
     inner_s;
     inner_s inner;
   };
-  double b;
+  int b;
 } outer_s;
 
-double inner_fn(inner_s p);
-double inner_fn(inner_s p) { return p.a * p.a; }
-
-double outer_fn(outer_s p);
-double outer_fn(outer_s p) { return p.a * p.a + p.b * p.b; }
-
 int main(void) {
-  outer_s demo = { .a = 3, .b = 0 };
-  printf("%g\n", outer_fn(demo));
-  printf("%g\n", inner_fn(demo.inner));
+  outer_s example = { .a = 3, .b = 0 };
+  return example.b;
 }
-
-#pragma clang diagnostic pop
