53ccbf6f77d56234d44fa6f4eef7267b397bcfd1
Sebastian Wiesner <lunaryorn@gmail.com>
2014-01-12 20:08:45 +0100

    Release 0.16

--- flycheck.el
+++ flycheck.el
@@ -5,7 +5,7 @@
 ;; Author: Sebastian Wiesner <lunaryorn@gmail.com>
 ;; URL: https://github.com/flycheck/flycheck
 ;; Keywords: convenience languages tools
-;; Version: 0.16-cvs
+;; Version: 0.16
 ;; Package-Requires: ((s "1.9.0") (dash "2.4.0") (f "0.11.0") (pkg-info "0.4") (cl-lib "0.3") (emacs "24.1"))
 
 ;; This file is not part of GNU Emacs.
