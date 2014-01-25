c09fc40ed5a2b51637ba1ae67db091ad46de7627
Sebastian Wiesner <lunaryorn@gmail.com>
2014-01-25 14:46:40 +0100

    Fix license
    test-helper.el isn't part of Emacs either

--- test/test-helper.el
+++ test/test-helper.el
@@ -2,6 +2,8 @@
 
 ;; Copyright (C) 2013, 2014  Sebastian Wiesner
 
+;; This file is not part of GNU Emacs.
+
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
