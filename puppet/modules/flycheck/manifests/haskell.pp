d91cc456c660e7b41ec743a393ddcaea8af2afe6
Sebastian Wiesner <lunaryorn@gmail.com>
2014-02-05 15:11:15 +0100

    Do not run cabal update every time

--- puppet/modules/flycheck/manifests/haskell.pp
+++ puppet/modules/flycheck/manifests/haskell.pp
@@ -7,6 +7,7 @@ class flycheck::haskell {
   exec { 'cabal update':
     environment => 'HOME=/root',
     path        => ['/usr/local/bin', '/usr/bin', '/bin'],
+    creates     => '/root/.cabal/packages/hackage.haskell.org/00-index.tar.gz',
     require     => Package['haskell-platform'],
   }
 }
