e0bf4acdea6a2d2514279fffb2018c38974852ca
Sebastian Wiesner <lunaryorn@gmail.com>
2014-02-08 14:19:13 +0100

    Do not install go vet explicitly
    It's part of the binary Go releases, so we do not need to install it
    again.

--- puppet/modules/flycheck/manifests/checkers/go.pp
+++ puppet/modules/flycheck/manifests/checkers/go.pp
@@ -4,13 +4,8 @@
 class flycheck::checkers::go {
 
   include flycheck::git         # For Github
-  include flycheck::mercurial   # For Mercurial on Google Code
 
   flycheck::go::get { 'github.com/golang/lint/golint': # go-golint
     require => Class['flycheck::git']
-  }
-
-  flycheck::go::get { 'code.google.com/p/go.tools/cmd/vet': # go-vet
-    require => Class['flycheck::mercurial']
   }
 }
--- puppet/modules/flycheck/manifests/mercurial.pp
+++
@@ -1,9 +0,0 @@
-# Class: flycheck::mercurial
-#
-# Install Mercurial
-class flycheck::mercurial {
-  package { 'mercurial':
-    ensure  => latest,
-    require => Class['apt::update'],
-  }
-}
