38173b62e71cb19ca0fed42d72f269a69bc0ca47
Sebastian Wiesner <lunaryorn@gmail.com>
2014-02-08 13:37:12 +0100

    Install Go 1.2 on the VM

--- puppet/modules/flycheck/manifests/checkers/go.pp
+++ puppet/modules/flycheck/manifests/checkers/go.pp
@@ -3,27 +3,14 @@
 # Install checkers with the Go package system.
 class flycheck::checkers::go {
 
-  include flycheck::go
-  include flycheck::git         # To install packages from Github
-  include flycheck::hg          # To install packages from Google Code
+  include flycheck::git         # For Github
+  include flycheck::mercurial   # For Mercurial on Google Code
 
-  exec { 'golint':
-    command     => 'go get github.com/golang/lint/golint',
-    path        => ['/usr/local/bin', '/usr/bin', '/bin'],
-    environment => ["GOPATH=${flycheck::go::gopath}"],
-    require     => [Class['flycheck::go', 'flycheck::git']],
+  flycheck::go::get { 'github.com/golang/lint/golint': # go-golint
+    require => Class['flycheck::git']
   }
 
-  file { '/usr/local/bin/golint':
-    ensure  => link,
-    target  => "${flycheck::go::gopath}/bin/golint",
-    require => Exec['golint']
-  }
-
-  exec { 'govet':
-    command     => 'go get code.google.com/p/go.tools/cmd/vet',
-    path        => ['/usr/local/bin', '/usr/bin', '/bin'],
-    environment => ["GOPATH=${flycheck::go::gopath}"],
-    require     => [Class['flycheck::go', 'flycheck::hg']],
+  flycheck::go::get { 'code.google.com/p/go.tools/cmd/vet': # go-vet
+    require => Class['flycheck::mercurial']
   }
 }
