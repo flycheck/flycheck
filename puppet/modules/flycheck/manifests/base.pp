d2eb3df83c21be565cb2637c8ab753ea982458aa
Sebastian Wiesner <lunaryorn@gmail.com>
2014-03-19 11:28:33 +0100

    Add comment to clarify the rist  apt::update dep cycle

--- puppet/modules/flycheck/manifests/base.pp
+++ puppet/modules/flycheck/manifests/base.pp
@@ -36,17 +36,18 @@ class flycheck::base {
     }
   }
 
-  # Required to add PPAs.  Install first, before re-configuring Apt, to avoid a
-  # dependency circle around apt::update
-  package { 'python-software-properties':
-    ensure  => latest,
-    require => [],
-    before  => Class['apt'],
-  }
-
   # Update Apt before installing packages
   Package {
     require => Class['apt::update']
+  }
+
+  package { 'python-software-properties':
+    ensure  => latest,
+    # Drop apt::update from Package default, to avoid a cyclic dependency on
+    # apt::update, because apt::update in turn will depend on this package,
+    # because it has the tools to add PPAs
+    require => [],
+    before  => Class['apt'],
   }
 
   # Important utilities
