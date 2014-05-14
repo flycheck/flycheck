flycheck/flycheck

commit 2e7130e1c883c790af73bdfa6eebb17dea893745

[Fix #387] Make temp prefix customizable
Also, change the default to .flycheck

flycheck.el
@@ -505,6 +505,13 @@ nil
                  (const :tag "Completing read" nil))
   :package-version '(flycheck . "0.17"))
 
+(defcustom flycheck-temp-prefix ".flycheck"
+  "Prefix for temporary files created by Flycheck."
+  :group 'flycheck
+  :type 'string
+  :package-version '(flycheck . "0.19")
+  :risky t)
+
 (defcustom flycheck-mode-hook nil
   "Hooks to run after `flycheck-mode'."
   :group 'flycheck
@@ -1104,51 +1111,56 @@ Otherwise return nil."
 (defvar-local flycheck-temporaries nil
   "Temporary files and directories created by Flycheck.")
 
-(defun flycheck-temp-dir-system (prefix)
-  "Create a unique temporary directory from PREFIX.
+(defun flycheck-temp-dir-system ()
+  "Create a unique temporary directory.
 
-Add the directory to `flycheck-temporaries'.
+Use `flycheck-temp-prefix' as prefix, and add the directory to
+`flycheck-temporaries'.
 
 Return the path of the directory"
-  (let* ((tempdir (make-temp-file prefix 'directory)))
+  (let* ((tempdir (make-temp-file flycheck-temp-prefix 'directory)))
     (push tempdir flycheck-temporaries)
     tempdir))
 
-(defun flycheck-temp-file-system (filename prefix)
-  "Create a temporary file named after FILENAME with PREFIX.
-
-If FILENAME is nil, this function creates a temporary file with
-PREFIX and a random suffix.  The path of the file is added to
-`flycheck-temporaries'.
+(defun flycheck-temp-file-system (filename )
+  "Create a temporary file named after FILENAME.
 
 If FILENAME is non-nil, this function creates a temporary
-directory with PREFIX and a random suffix using
-`flycheck-temp-dir-system', and creates a file with the same name
-as FILENAME in this directory
+directory with `flycheck-temp-dir-system', and creates a file
+with the same name as FILENAME in this directory.
+
+Otherwise this function creates a temporary file with
+`flycheck-temp-prefix' and a random suffix.  The path of the file
+is added to `flycheck-temporaries'.
+
+Add the path of the file to `flycheck-temporaries'.
 
 Return the path of the file."
   (let (tempfile)
     (if filename
-        (let ((directory (flycheck-temp-dir-system prefix)))
+        (let ((directory (flycheck-temp-dir-system)))
           (setq tempfile (f-join directory (f-filename filename))))
-      (setq tempfile (make-temp-file prefix)))
+      (setq tempfile (make-temp-file flycheck-temp-prefix)))
     (push tempfile flycheck-temporaries)
     tempfile))
 
-(defun flycheck-temp-file-inplace (filename prefix)
-  "Create an in-place copy of FILENAME with PREFIX added.
+(defun flycheck-temp-file-inplace (filename)
+  "Create an in-place copy of FILENAME.
 
-Add the path of the file to `flycheck-temporaries'.
+Prefix the file with `flycheck-temp-prefix' and add the path of
+the file to `flycheck-temporaries'.
 
 If FILENAME is nil, fall back to `flycheck-temp-file-system'.
 
 Return the path of the file."
   (if filename
-      (let* ((tempname (format "%s_%s" prefix (f-filename filename)))
+      (let* ((tempname (format "%s_%s"
+                               flycheck-temp-prefix
+                               (f-filename filename)))
              (tempfile (f-expand (f-join (f-dirname filename) tempname))))
         (push tempfile flycheck-temporaries)
         tempfile)
-    (flycheck-temp-file-system filename prefix)))
+    (flycheck-temp-file-system filename)))
 
 (defun flycheck-save-buffer-to-file (file-name)
   "Save the contents of the current buffer to FILE-NAME."
@@ -1156,11 +1168,11 @@ Return the path of the file."
   (let ((jka-compr-inhibit t))
     (write-region nil nil file-name nil 0)))
 
-(defun flycheck-save-buffer-to-temp (temp-file-fn prefix)
-  "Save buffer to temp file returned by TEMP-FILE-FN with PREFIX.
+(defun flycheck-save-buffer-to-temp (temp-file-fn)
+  "Save buffer to temp file returned by TEMP-FILE-FN.
 
 Return the name of the temporary file."
-  (let ((filename (funcall temp-file-fn (buffer-file-name) prefix)))
+  (let ((filename (funcall temp-file-fn (buffer-file-name))))
     ;; Do not flush short-lived temporary files onto disk
     (let ((write-region-inhibit-fsync t))
       (flycheck-save-buffer-to-file filename))
@@ -1933,13 +1945,13 @@ are substituted within the body of cells!"
   (pcase arg
     ((pred stringp) arg)
     (`source
-     (flycheck-save-buffer-to-temp #'flycheck-temp-file-system "flycheck"))
+     (flycheck-save-buffer-to-temp #'flycheck-temp-file-system))
     (`source-inplace
-     (flycheck-save-buffer-to-temp #'flycheck-temp-file-inplace "flycheck"))
+     (flycheck-save-buffer-to-temp #'flycheck-temp-file-inplace))
     (`source-original (or (buffer-file-name) ""))
-    (`temporary-directory (flycheck-temp-dir-system "flycheck"))
+    (`temporary-directory (flycheck-temp-dir-system))
     (`temporary-file-name
-     (let ((directory (flycheck-temp-dir-system "flycheck")))
+     (let ((directory (flycheck-temp-dir-system)))
        (make-temp-name (f-join directory "flycheck"))))
     (`(config-file ,option-name ,file-name-var)
      (-when-let* ((value (symbol-value file-name-var))
@@ -4338,7 +4350,7 @@ is added to the GHC search path via `-i'."
   "A Haskell syntax and type checker using ghc.
 
 See URL `http://www.haskell.org/ghc/'."
-  :command ("ghc" "-Wall" "-fno-code"
+  :command ("ghc" "-Wall" "-fno-code" "-c"
             (option-flag "-no-user-package-db"
                          flycheck-ghc-no-user-package-database)
             (option-list "-package-db" flycheck-ghc-package-databases)

test/flycheck-test.el
@@ -500,6 +500,10 @@ check with.  ERRORS is the list of expected errors."
     (ignore-errors
       (custom-set-variables '(flycheck-keymap-prefix (kbd "C-c !"))))))
 
+(ert-deftest flycheck-temp-prefix/default ()
+  :tags '(customization)
+  (should (equal flycheck-temp-prefix ".flycheck")))
+
 
 ;;;; Minor mode definition
 
@@ -971,28 +975,27 @@ check with.  ERRORS is the list of expected errors."
 
 (ert-deftest flycheck-temp-dir-system ()
   :tags '(utility)
-  (let ((dirname (flycheck-temp-dir-system "flycheck-test")))
+  (let ((dirname (flycheck-temp-dir-system)))
     (unwind-protect
         (should (f-directory? dirname))
       (flycheck-safe-delete-temporaries))
     (should-not (f-exists? dirname))
     (should (f-child-of? dirname temporary-file-directory))
-    (should (s-starts-with? "flycheck-test" (f-filename dirname)))))
+    (should (s-starts-with? flycheck-temp-prefix (f-filename dirname)))))
 
 (ert-deftest flycheck-temp-file-system/without-file-name ()
   :tags '(utility)
-  (let ((filename (flycheck-temp-file-system nil "flycheck-test")))
+  (let ((filename (flycheck-temp-file-system nil)))
     (unwind-protect
         (should (f-file? filename))
       (flycheck-safe-delete-temporaries))
     (should-not (f-exists? filename))
     (should (f-child-of? filename temporary-file-directory))
-    (should (s-starts-with? "flycheck-test" (f-filename filename)))))
+    (should (s-starts-with? flycheck-temp-prefix (f-filename filename)))))
 
 (ert-deftest flycheck-temp-file-system/with-complete-path ()
   :tags '(utility)
-  (let* ((filename (flycheck-temp-file-system "spam/with/eggs.el"
-                                              "flycheck-test"))
+  (let* ((filename (flycheck-temp-file-system "spam/with/eggs.el"))
          (dirname (directory-file-name (f-parent filename))))
     (unwind-protect
         (progn
@@ -1006,37 +1009,38 @@ check with.  ERRORS is the list of expected errors."
     ;; subdirectory of the temporary directory
     (should (string= "eggs.el" (f-filename filename)))
     (should (f-child-of? dirname temporary-file-directory))
-    (should (s-starts-with? "flycheck-test" (f-filename dirname)))))
+    (should (s-starts-with? flycheck-temp-prefix (f-filename dirname)))))
 
 (ert-deftest flycheck-temp-file-inplace/with-just-basename ()
   :tags '(utility)
   (let* ((default-directory flycheck-test-directory)
-         (filename (flycheck-temp-file-inplace "eggs.el" "flycheck-test")))
+         (filename (flycheck-temp-file-inplace "eggs.el")))
     (unwind-protect
         ;; In place files should not be created early
         (should-not (f-exists? filename))
       (flycheck-safe-delete-temporaries))
-    (should (string= filename (f-expand "flycheck-test_eggs.el")))))
+    (should (string= filename (f-expand (concat flycheck-temp-prefix
+                                                "_eggs.el"))))))
 
 (ert-deftest flycheck-temp-file-inplace/with-complete-path ()
   :tags '(utility)
   (let* ((default-directory flycheck-test-directory)
-         (filename (flycheck-temp-file-inplace "spam/with/eggs.el"
-                                               "flycheck-test")))
+         (filename (flycheck-temp-file-inplace "spam/with/eggs.el")))
     (unwind-protect
         (should-not (f-exists? filename))
       (flycheck-safe-delete-temporaries))
     (should (string= filename
-                     (f-expand (f-join "spam/with" "flycheck-test_eggs.el"))))))
+                     (f-expand (f-join "spam/with" (concat flycheck-temp-prefix
+                                                           "_eggs.el")))))))
 
 (ert-deftest flycheck-temp-file-inplace/without-file-name ()
   :tags '(utility)
-  (let ((filename (flycheck-temp-file-inplace nil "flycheck-test")))
+  (let ((filename (flycheck-temp-file-inplace nil)))
     (unwind-protect
         (should (f-file? filename))
       (flycheck-safe-delete-temporaries))
     (should-not (file-name-extension filename))
-    (should (s-starts-with? "flycheck-test" (f-filename filename)))))
+    (should (s-starts-with? flycheck-temp-prefix (f-filename filename)))))
 
 (ert-deftest flycheck-save-buffer-to-file ()
   :tags '(utility)
@@ -1179,7 +1183,7 @@ check with.  ERRORS is the list of expected errors."
 
 (ert-deftest flycheck-safe-delete/recursive-removal ()
   :tags '(utility)
-  (let ((dirname (flycheck-temp-dir-system "flycheck-test")))
+  (let ((dirname (flycheck-temp-dir-system)))
     (unwind-protect
         (let ((filename (f-join dirname "foo")))
           (process-lines "touch" filename)
@@ -1478,7 +1482,8 @@ check with.  ERRORS is the list of expected errors."
 
           (let ((filename (flycheck-substitute-argument 'source-inplace 'emacs-lisp)))
             (should (equal filename (flycheck-test-resource-filename
-                                     "flycheck_substitute-dummy")))
+                                     (concat flycheck-temp-prefix
+                                             "_substitute-dummy"))))
             (should (f-exists? filename)))
 
           (let ((filename (flycheck-substitute-argument 'source 'emacs-lisp)))

commit 095150e8a34f0305b97a77b426ca89f8e9719cd2

Remove wrong GHC argument
Regression from 2e7130e

flycheck.el
@@ -4350,7 +4350,7 @@ is added to the GHC search path via `-i'."
   "A Haskell syntax and type checker using ghc.
 
 See URL `http://www.haskell.org/ghc/'."
-  :command ("ghc" "-Wall" "-fno-code" "-c"
+  :command ("ghc" "-Wall" "-fno-code"
             (option-flag "-no-user-package-db"
                          flycheck-ghc-no-user-package-database)
             (option-list "-package-db" flycheck-ghc-package-databases)