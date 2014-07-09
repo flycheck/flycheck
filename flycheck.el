7a47a55fec06417c3d20ce26dd61b308922c1c11
Per Nordlöw <per.nordlow@gmail.com>
2014-07-10 00:19:13 +0200

    Always use d-dscanner-style after d-dscanner-syntax because dscanner --syntaxCheck may give false positives

--- flycheck.el
+++ flycheck.el
@@ -4723,7 +4723,7 @@ See URL `https://github.com/Hackerpilot/Dscanner/'."
   ((error line-start (file-name) "(" line ":" column ")[error]: " (message) line-end)
    (warning line-start (file-name) "(" line ":" column ")[warn]: " (message) line-end))
   :modes d-mode
-  :next-checkers ((no-errors . d-dscanner-style)))
+  :next-checkers (d-dscanner-style))
 
 (flycheck-define-checker d-dscanner-style
   "A D style checker using Dscanner.
