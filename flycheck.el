24b4063c78298a9eeddb50283b151fb59e0f3ec9
Per Nordlöw <per.nordlow@gmail.com>
2014-07-10 15:45:32 +0200

    Fix :error-patterns for fortran-gcc

--- flycheck.el
+++ flycheck.el
@@ -4418,8 +4418,12 @@ Uses GCC's Fortran compiler gfortran.  See URL `https://gcc.gnu.org/'."
             ;; with quotes
             source-inplace)
   :error-patterns
-  ((error line-start (file-name) ":" line "." column ":"
-          "Error" ": " (message) line-end))
+  ((error line-start (file-name) ":" line "." column ":\n"
+          (= 3 (zero-or-more not-newline) "\n")
+          (or "Error" "Fatal Error") ": " (message) line-end)
+   (warning line-start (file-name) ":" line "." column ":\n"
+            (= 3 (zero-or-more not-newline) "\n")
+            "Warning: " (message) line-end))
   :modes (fortran-mode f90-mode))
 
 (flycheck-def-option-var flycheck-cppcheck-checks '("style") c/c++-cppcheck
