b01f3d8f909359b57bc413ba0eb39faaaee7fa93
Sebastian Wiesner <swiesner@lunaryorn.com>
2014-07-11 07:54:11 +0200

    Add FSF copyright
    Since we copy some code from Emacs for backwards compatibility, we must
    preserve the copyright notice of the FSF

--- flycheck.el
+++ flycheck.el
@@ -1,6 +1,7 @@
 ;;; flycheck.el --- Modern on-the-fly syntax checking for GNU Emacs -*- lexical-binding: t; -*-
 
 ;; Copyright (c) 2012, 2013, 2014 Sebastian Wiesner <swiesner@lunaryorn.com>
+;; Copyright (C) 2013, 2014 Free Software Foundation, Inc.
 ;;
 ;; Author: Sebastian Wiesner <swiesner@lunaryorn.com>
 ;; URL: https://flycheck.readthedocs.org
