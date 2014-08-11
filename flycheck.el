3fdb308682a77ff4d121379f46d9b4e3b6641ab4
Sebastian Wiesner <swiesner@lunaryorn.com>
2014-08-11 23:02:16 +0200

    Use a proper warning

--- flycheck.el
+++ flycheck.el
@@ -2224,7 +2224,7 @@ otherwise."
       (and (flycheck-check-modes checker)
            (flycheck-check-predicate checker)
            (flycheck-check-executable checker))
-    (message "Warning: %S is no valid Flycheck syntax checker.
+    (lwarn 'flycheck :warning "Warning: %S is no valid Flycheck syntax checker.
 Try to reinstall the package defining this syntax checker." checker)
     nil))
 
