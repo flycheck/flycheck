7cadddcec02d76213690b3ef4517d1a4a482cc0e
Per Nordlöw <per.nordlow@gmail.com>
2014-08-23 13:44:21 +0200

    Remove use of d-dscanner-syntax for now

--- flycheck.el
+++ flycheck.el
@@ -4710,8 +4710,7 @@ Requires DMD 2.066 or newer.  See URL `http://dlang.org/'."
    (warning line-start (file-name) "(" line "," column "): "
             (or "Warning" "Deprecation") ": " (message) line-end))
   :modes d-mode
-  :next-checkers ((warnings-only . d-dscanner-syntax)
-                  (warnings-only . d-dscanner-style)))
+  :next-checkers ((warnings-only . d-dscanner-style)))
 
 (flycheck-define-checker d-dscanner-syntax
   "A D syntax checker using Dscanner.
