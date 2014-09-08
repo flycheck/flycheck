flycheck/flycheck

commit 7dccd8ae36396352c55eed5da10e08acc666c35c

Update to recent Google This versions

flycheck.el
@@ -3987,7 +3987,7 @@ of `google-wrap-in-quotes'.
 This function requires the Google This library from URL
 `https://github.com/Bruce-Connor/emacs-google-this'."
   (interactive "d\nP")
-  (if (fboundp 'google-string)
+  (if (fboundp 'google-this-string)
       (let ((messages (delq nil (mapcar #'flycheck-error-message
                                         (flycheck-overlay-errors-at pos)))))
         (when (and flycheck-google-max-messages

commit 00509fd86811aa708a418934bb76eb0e764364bd

Call the autoloaded Google This function
Regression from 7dccd8a

flycheck.el
@@ -3995,7 +3995,7 @@ This function requires the Google This library from URL
           (user-error "More than %s messages at point"
                       flycheck-google-max-messages))
         (dolist (msg messages)
-          (google-string quote-flag msg 'no-confirm)))
+          (google-this-string quote-flag msg 'no-confirm)))
     (user-error "Please install Google This from \
 https://github.com/Bruce-Connor/emacs-google-this")))
 