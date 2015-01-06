flycheck/flycheck

commit c5875a7cc9f2d0a9d63981312d1d1ac8e0215d4c

[Fix #559] Don't cache last checker without using it
Only cache the last used syntax checker when actually using it, to make
flycheck-get-checker-for-buffer free of side effects.

Specifically Flycheck calls this function to determine whether it may
enable Flycheck Mode in a particular buffer when Global Flycheck Mode is
on.  In Emacs 24.3 and earlier, this happens at a point where hooks have
not yet run, which in turn affects the predicates of registered syntax
checkers, if they check for specific major modes.

As a result, a syntax checker would have been cached as last checker and
used for the buffer subsequently, even if a preferred checker was
available later, if a minor mode was enabled through hooks.

flycheck.el
@@ -2023,8 +2023,7 @@ Return the checker if there is any, or nil otherwise."
   (let ((checkers flycheck-checkers))
     (while (and checkers (not (flycheck-may-use-checker (car checkers))))
       (setq checkers (cdr checkers)))
-    (when checkers
-      (setq flycheck-last-checker (car checkers)))))
+    (car checkers)))
 
 (defun flycheck-get-checker-for-buffer ()
   "Find the checker for the current buffer.
@@ -2137,6 +2136,9 @@ Set `flycheck-current-syntax-check' accordingly."
             (let* ((checker (flycheck-get-checker-for-buffer)))
               (if checker
                   (progn
+                    ;; Remember the last syntax checker to speed up checker
+                    ;; selection
+                    (setq flycheck-last-checker checker)
                     (flycheck-start-current-syntax-check checker)
                     (flycheck-report-status 'running))
                 (flycheck-clear)

commit ebb69cfe68d3a4847c72031954bdc65efaa5859e

Update documentation
Regression from c5875a7, thanks @Sarcasm

flycheck.el
@@ -2016,9 +2016,6 @@ Return the checker if it may be used, or nil otherwise."
 (defun flycheck-get-new-checker-for-buffer ()
   "Find a new checker for the current buffer.
 
-If a checker is found set `flycheck-last-checker' to re-use this
-checker for the next check.
-
 Return the checker if there is any, or nil otherwise."
   (let ((checkers flycheck-checkers))
     (while (and checkers (not (flycheck-may-use-checker (car checkers))))
@@ -2122,7 +2119,11 @@ Set `flycheck-current-syntax-check' accordingly."
            syntax-check args)))
 
 (defun flycheck-buffer ()
-  "Check syntax in the current buffer."
+  "Check syntax in the current buffer.
+
+The syntax checker used for the syntax check is cached in
+`flycheck-last-checker' and re-used for the next check, if
+possible."
   (interactive)
   (flycheck-clean-deferred-check)
   (if flycheck-mode