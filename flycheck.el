0bdc33d46cd2df2ff2095b720466d5e1cf8bc69a
yzprofile <yzprofiles@gmail.com>
2015-06-21 05:04:26 +0800

    Add flycheck-erlang-include-path variable

--- flycheck.el
+++ flycheck.el
@@ -5886,11 +5886,25 @@ The checker runs `checkdoc-current-buffer'."
   (setcar (get 'emacs-lisp-checkdoc 'flycheck-command)
           flycheck-this-emacs-executable))
 
+(flycheck-def-option-var flycheck-erlang-include-path nil erlang
+  "Whether to error on language extensions in Erlang.
+
+The value of this variable is a list of strings, where each
+string is a directory to add to the include path of erlang.
+Relative paths are relative to the file being checked."
+  :type '(repeat (directory :tag "Include directory"))
+  :safe #'flycheck-string-list-p
+  :package-version '(flycheck . "0.24"))
+
 (flycheck-define-checker erlang
   "An Erlang syntax checker using the Erlang interpreter.
 
 See URL `http://www.erlang.org/'."
-  :command ("erlc" "-o" temporary-directory "-Wall" source)
+  :command ("erlc"
+            "-o" temporary-directory
+            (option-list "-I" flycheck-erlang-include-path)
+            "-Wall"
+            source)
   :error-patterns
   ((warning line-start (file-name) ":" line ": Warning:" (message) line-end)
    (error line-start (file-name) ":" line ": " (message) line-end))
