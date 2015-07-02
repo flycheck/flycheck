583fcb78323354e2f06323e826e2653ddd1d5c78
Sebastian Wiesner <swiesner@lunaryorn.com>
2015-07-02 08:50:40 +0200

    Deploy manual during script phase
    Makes sure that deployment failures fail the build

--- travis.mk
+++ travis.mk
@@ -27,8 +27,7 @@ export EMACS
 .PHONY:  deps compile test texinfo deploy_manual \
 	before_install before_install_unit before_install_emacs \
 	install install_unit install_manual \
-	script script_unit script_manual \
-	after_success after_success_unit after_success_manual
+	script script_unit script_manual
 
 # SUPPORT TARGETS
 install_emacs:
@@ -59,7 +58,7 @@ texinfo:
 	makeinfo --version
 	make texinfo
 
-deploy_manual:
+deploy_manual: texinfo
 	bash doc/deploy-travis.bash
 
 # TARGETS FOR TRAVIS PHASES
@@ -77,12 +76,6 @@ install: install_$(TRAVIS_BUILD)
 
 script_unit: test
 
-script_manual: texinfo
+script_manual: deploy_manual
 
 script: script_$(TRAVIS_BUILD)
-
-after_success_unit:
-
-after_success_manual: deploy_manual
-
-after_success: after_success_$(TRAVIS_BUILD)
