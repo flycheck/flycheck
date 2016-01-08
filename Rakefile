flycheck/flycheck

commit f08af94ea99ffbdee6763eec0da0fde86610b180

Move checkdoc runner to admin/

Rakefile
@@ -122,7 +122,7 @@ namespace :verify do
   desc 'Verify Emacs Lisp sources'
   task :elisp do
     sh(*emacs_batch('--eval', '(setq checkdoc-arguments-in-order-flag nil)',
-                    '-l', 'test/flycheck-checkdoc.el',
+                    '-l', 'test/run-checkdoc.el',
                     '-f', 'flycheck-checkdoc-batch-and-exit',
                     *SOURCES))
   end

admin/run-checkdoc.el
@@ -1,6 +1,6 @@
-;;; flycheck-checkdoc.el --- Flycheck: Checkdoc linter -*- lexical-binding: t; -*-
+;;; run-checkdoc.el --- Flycheck: Run checkdoc on Flycheck -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2015  Sebastian Wiesner
+;; Copyright (C) 2015-2016  Sebastian Wiesner
 
 ;; Author: Sebastian Wiesner <swiesner@lunaryorn.com>
 ;; Maintainer: Sebastian Wiesner <swiesner@lunaryorn.com>
@@ -52,5 +52,4 @@
   (mapc #'flycheck-checkdoc-file-batch command-line-args-left)
   (kill-emacs 0))
 
-(provide 'flycheck-checkdoc)
-;;; flycheck-checkdoc.el ends here
+;;; run-checkdoc.el ends here

test/flycheck-test.el
@@ -120,8 +120,8 @@ and extension, as in `file-name-base'."
 (flycheck-test-def-indent-test "flycheck.el")
 (flycheck-test-def-indent-test "flycheck-ert.el")
 (flycheck-test-def-indent-test "test/run.el")
-(flycheck-test-def-indent-test "test/flycheck-checkdoc.el")
 (flycheck-test-def-indent-test "test/flycheck-test.el")
+(flycheck-test-def-indent-test "admin/run-checkdoc.el")
 
 (defmacro flycheck-test-def-untabified-test (filename)
   "Define a test case for the absence of tabs in FILENAME.
@@ -145,8 +145,8 @@ and extension, as in `file-name-base'."
 (flycheck-test-def-untabified-test "flycheck.el")
 (flycheck-test-def-untabified-test "flycheck-ert.el")
 (flycheck-test-def-untabified-test "test/run.el")
-(flycheck-test-def-untabified-test "test/flycheck-checkdoc.el")
 (flycheck-test-def-untabified-test "test/flycheck-test.el")
+(flycheck-test-def-untabified-test "test/run-checkdoc.el")
 
 
 ;;; Manual

commit 9f2983af0a71a2fdc7ed572cdf92c3c1433d668d

Fix checkdoc invocation in Rakefile
Regression from f08af94ea99ffbdee6763eec0da0fde86610b180 :(

Rakefile
@@ -122,7 +122,7 @@ namespace :verify do
   desc 'Verify Emacs Lisp sources'
   task :elisp do
     sh(*emacs_batch('--eval', '(setq checkdoc-arguments-in-order-flag nil)',
-                    '-l', 'test/run-checkdoc.el',
+                    '-l', 'admin/run-checkdoc.el',
                     '-f', 'flycheck-checkdoc-batch-and-exit',
                     *SOURCES))
   end