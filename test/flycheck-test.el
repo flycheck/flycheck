cdfe41afa6d93b2feae35de3545d2d63d9678e71
Mario Rodas <marsam@users.noreply.github.com>
2016-04-03 16:30:30 -0500

    Add proselint checker

--- test/flycheck-test.el
+++ test/flycheck-test.el
@@ -3625,6 +3625,19 @@ Why not:
    '(26 12 error "TRUE, FALSE and NULL must be lowercase; expected \"false\" but found \"FALSE\""
         :id "Generic.PHP.LowerCaseConstant.Found" :checker php-phpcs)))
 
+(flycheck-ert-def-checker-test proselint (text markdown) nil
+  (flycheck-ert-should-syntax-check
+   "language/text.txt" 'text-mode
+   '(1 7 warning "Substitute 'damn' every time you're inclined to write 'very;' your editor will delete it and the writing will be just as it should be."
+       :id "weasel_words.very"
+       :checker proselint)
+   '(2 4 warning "Redundancy. Use 'associate' instead of 'associate together'."
+       :id "redundancy.garner"
+       :checker proselint)
+   '(3 5 warning "Gender bias. Use 'lawyer' instead of 'lady lawyer'."
+       :id "sexism.misc"
+       :checker proselint)))
+
 (flycheck-ert-def-checker-test processing processing syntax-error
   (flycheck-ert-should-syntax-check
    "language/processing/syntax_error/syntax_error.pde" 'processing-mode
