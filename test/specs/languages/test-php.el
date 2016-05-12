49c4f3fc1798ba4b9e0ed09619a591d183d0282d
Sebastian Wiesner <swiesner@lunaryorn.com>
2016-05-12 16:15:51 +0200

    Refine PHPMD error parser spec

--- test/specs/languages/test-php.el
+++ test/specs/languages/test-php.el
@@ -25,8 +25,11 @@
 
 (require 'flycheck-buttercup)
 
-(defconst flycheck/phpmd-xml
-  "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>
+(describe "Language PHP"
+  (describe "The PHDMD error parser"
+    (it "parses PHPMD XML output"
+
+      (let ((phpmd-xml  "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>
 <pmd version=\"1.5.0\" timestamp=\"2014-12-02T18:13:44+00:00\">
   <file name=\"foo.php\">
     <violation beginline=\"21\" endline=\"21\" rule=\"UnusedPrivateField\"
@@ -48,32 +51,27 @@
         Avoid unused parameters such as '$baz'.
     </violation>
   </file>
-</pmd>"
-  "Example phpmd output.")
-
-(describe "Language PHP"
-  (describe "PHP Mess Detector checker"
-    (it "parses PHPMD XML output"
-      (expect (flycheck-parse-phpmd flycheck/phpmd-xml 'checker 'buffer)
-              :to-be-equal-flycheck-errors
-              (list
-               (flycheck-error-new-at 21 nil 'warning
-                                      "Avoid unused private fields such as '$FOO'."
-                                      :id "UnusedPrivateField"
-                                      :checker 'checker
-                                      :buffer 'buffer
-                                      :filename "foo.php")
-               (flycheck-error-new-at 24 nil 'warning
-                                      "Avoid unused private methods such as 'bar'."
-                                      :id "UnusedPrivateMethod"
-                                      :checker 'checker
-                                      :buffer 'buffer
-                                      :filename "foo.php")
-               (flycheck-error-new-at 24 nil 'warning
-                                      "Avoid unused parameters such as '$baz'."
-                                      :id "UnusedFormalParameter"
-                                      :checker 'checker
-                                      :buffer 'buffer
-                                      :filename "foo.php"))))))
+</pmd>"))
+        (expect (flycheck-parse-phpmd phpmd-xml 'checker 'buffer)
+                :to-be-equal-flycheck-errors
+                (list
+                 (flycheck-error-new-at 21 nil 'warning
+                                        "Avoid unused private fields such as '$FOO'."
+                                        :id "UnusedPrivateField"
+                                        :checker 'checker
+                                        :buffer 'buffer
+                                        :filename "foo.php")
+                 (flycheck-error-new-at 24 nil 'warning
+                                        "Avoid unused private methods such as 'bar'."
+                                        :id "UnusedPrivateMethod"
+                                        :checker 'checker
+                                        :buffer 'buffer
+                                        :filename "foo.php")
+                 (flycheck-error-new-at 24 nil 'warning
+                                        "Avoid unused parameters such as '$baz'."
+                                        :id "UnusedFormalParameter"
+                                        :checker 'checker
+                                        :buffer 'buffer
+                                        :filename "foo.php")))))))
 
 ;;; test-php.el ends here
