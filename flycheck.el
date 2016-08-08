ef68693f71637ce63f69f4b5dc9cdac61205708e
Sergey Vinokurov <serg.foo@gmail.com>
2016-08-08 23:43:07 +0300

    Review fixes

--- flycheck.el
+++ flycheck.el
@@ -5264,16 +5264,14 @@ function `buffer-file-name'."
       (when (seq-some (apply-partially #'flycheck-same-files-p
                                        (expand-file-name filename cwd))
                       buffer-files)
-        (let ((new-filename (buffer-file-name)))
+        (-when-let (new-filename (buffer-file-name))
           (setf (flycheck-error-filename err) new-filename
-                (flycheck-error-message err)
-                (replace-regexp-in-string
-                 (regexp-quote filename)
-                 new-filename
-                 (flycheck-error-message err)
-                 nil
-                 t ;; do literal substitution
-                 ))))))
+                (flycheck-error-message err) (replace-regexp-in-string
+                                              (regexp-quote filename)
+                                              new-filename
+                                              (flycheck-error-message err)
+                                              'fixed-case
+                                              'literal))))))
   err)
 
 
