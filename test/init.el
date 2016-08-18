d0a2aa9b677bac443e8a4f4d61e8a9173630ef7d
Sebastian Wiesner <swiesner@lunaryorn.com>
2016-08-18 17:35:48 +0200

    Don’t trigger side-effects when loading test/init.el
    A necessary prerequisite to be able to check formatting for all files
    generically, as this requires loading files to make indentation rules
    for local definitions available.
    
    And in any case, doing stuff on top level is bad style for anything
    other than the real init file.

--- test/init.el
+++ test/init.el
@@ -23,8 +23,8 @@
 
 ;;; Commentary:
 
-;; Use with `emacs -Q -l test/init.el' to start a clean session with only
-;; Flycheck present.
+;; Use with `emacs -Q -l test/init.el -f flycheck/init' to start a clean session
+;; with only Flycheck present.
 ;;
 ;; Installs all dependencies of Flycheck to a temporary package directory, and
 ;; loads Flycheck.
@@ -33,76 +33,91 @@
 
 (require 'package)
 
-(setq load-prefer-newer t)              ; Don't load outdated bytecode
+(defun flycheck/init ()
+  "Initialize a minimal Flycheck session.
 
-(setq package-user-dir (expand-file-name "init-elpa"
-                                         (file-name-directory load-file-name))
-      package-check-signature nil)
-(add-to-list 'package-archives '("MELPA" . "https://stable.melpa.org/packages/"))
+Install Flycheck and its dependencies into a separate package
+directory (init-elpa in the current directory) and enable
+Flycheck globally.
 
-(package-initialize)
+Set some global customization options to get rid of some of the
+worst defaults of Emacs and create a half-way bearable Emacs
+session.
 
-(let* ((source-dir (locate-dominating-file load-file-name "flycheck.el"))
-       (flycheck-el (expand-file-name "flycheck.el" source-dir)))
-  ;; Install Flycheck to bring its dependencies in
-  (unless (package-installed-p 'flycheck)
-    (package-refresh-contents)
-    (package-install-file flycheck-el))
-  (load flycheck-el))
+Define a `demo' checker which can be used to demonstrate Flycheck
+in this file."
+  (setq load-prefer-newer t)              ; Don't load outdated bytecode
 
-(require 'flycheck)
-(global-flycheck-mode)
+  (setq package-user-dir (expand-file-name "init-elpa"
+                                           (file-name-directory load-file-name))
+        package-check-signature nil)
+  (add-to-list 'package-archives '("MELPA" . "https://stable.melpa.org/packages/"))
 
-(list 'an-info-here
-      'a-warning-here
-      'an-error-here)
+  (package-initialize)
 
-;; Some little convenience, to this Emacs session at least half way bearable
-(require 'ido)
-(ido-mode t)
-(setq ido-enable-flex-matching t)
+  (let* ((source-dir (locate-dominating-file load-file-name "flycheck.el"))
+         (flycheck-el (expand-file-name "flycheck.el" source-dir)))
+    ;; Install Flycheck to bring its dependencies in
+    (unless (package-installed-p 'flycheck)
+      (package-refresh-contents)
+      (package-install-file flycheck-el))
+    (load flycheck-el))
 
-;; Get rid of all the silly UI clutter of a default Emacs session and opt out of
-;; all the stupid startup and license messages
-(tool-bar-mode -1)
-(blink-cursor-mode -1)
-(setq ring-bell-function #'ignore
-      inhibit-startup-screen t
-      initial-scratch-message "")
-(fset 'yes-or-no-p #'y-or-n-p)
-(fset 'display-startup-echo-area-message #'ignore)
+  (require 'flycheck)
+  (global-flycheck-mode)
 
-;; Improve OS X key behaviour
-(when (eq system-type 'darwin)
-  (setq mac-option-modifier 'meta       ; Option is simply the natural Meta
-        mac-command-modifier 'meta      ; But command is a lot easier to hit
-        mac-right-command-modifier 'left
-        mac-right-option-modifier 'none ; Keep right option for accented input
-        mac-function-modifier 'hyper))
+  ;; Some dummy code for warnings which we use to demonstrate Flycheck, see
+  ;; `demo' checker below.
+  (list 'an-info-here
+        'a-warning-here
+        'an-error-here)
 
-(flycheck-define-generic-checker 'demo
-  "A demo syntax checker."
-  :start (lambda (checker callback)
-           (funcall callback 'finished
-                    (save-excursion
-                      (goto-char (point-min))
-                      (search-forward "an-info-here")
-                      (list (flycheck-error-new-at (line-number-at-pos)
-                                                   10 'info "An info here"
-                                                   :checker checker)
-                            (flycheck-error-new-at (+ 1 (line-number-at-pos))
-                                                   10 'warning "A warning here"
-                                                   :checker checker)
-                            (flycheck-error-new-at (+ 2 (line-number-at-pos))
-                                                   10 'error "A error here"
-                                                   :checker checker)))))
-  :modes 'emacs-lisp-mode)
+  ;; Some little convenience, to this Emacs session at least half way bearable
+  (require 'ido)
+  (ido-mode t)
+  (setq ido-enable-flex-matching t)
+
+  ;; Get rid of all the silly UI clutter of a default Emacs session and opt out of
+  ;; all the stupid startup and license messages
+  (tool-bar-mode -1)
+  (blink-cursor-mode -1)
+  (setq ring-bell-function #'ignore
+        inhibit-startup-screen t
+        initial-scratch-message "")
+  (fset 'yes-or-no-p #'y-or-n-p)
+  (fset 'display-startup-echo-area-message #'ignore)
+
+  ;; Improve OS X key behaviour
+  (when (eq system-type 'darwin)
+    (setq mac-option-modifier 'meta       ; Option is simply the natural Meta
+          mac-command-modifier 'meta      ; But command is a lot easier to hit
+          mac-right-command-modifier 'left
+          mac-right-option-modifier 'none ; Keep right option for accented input
+          mac-function-modifier 'hyper))
+
+  (flycheck-define-generic-checker 'demo
+    "A demo syntax checker."
+    :start (lambda (checker callback)
+             (funcall callback 'finished
+                      (save-excursion
+                        (goto-char (point-min))
+                        (search-forward "an-info-here")
+                        (list (flycheck-error-new-at (line-number-at-pos)
+                                                     10 'info "An info here"
+                                                     :checker checker)
+                              (flycheck-error-new-at (+ 1 (line-number-at-pos))
+                                                     10 'warning "A warning here"
+                                                     :checker checker)
+                              (flycheck-error-new-at (+ 2 (line-number-at-pos))
+                                                     10 'error "A error here"
+                                                     :checker checker)))))
+    :modes 'emacs-lisp-mode))
 
 (defun flycheck-prepare-screenshot (&optional hide-cursor)
   "Prepare this Emacs session for a screenshot.
 
-If HIDE-CURSOR is non-nil, or with prefix arg, hide the cursor,
-otherwise keep it."
+If HIDE-CURSOR is non-nil or if a prefix arg is given hide the
+cursor, otherwise keep it."
   (interactive "P")
   ;; Reduce UI and disable cursor
   (menu-bar-mode -1)
@@ -121,5 +136,9 @@ otherwise keep it."
                  (side            . bottom)
                  (reusable-frames . visible)
                  (window-height   . 0.33))))
+
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
 
 ;;; init.el ends here
