1bfccbdfa5fd70b58515e2192bb7615162bbb671
Wouter Bolsterlee <wouter@bolsterl.ee>
2016-08-18 19:26:16 +0200

    Immediately show errors after explicit jumps
    Immediately show any errors at point after using
    flycheck-{first,next,previous}-error. Fixes #1034.

--- flycheck.el
+++ flycheck.el
@@ -3706,7 +3706,8 @@ position."
   (when (consp n)
     ;; Universal prefix argument means reset
     (setq reset t n nil))
-  (flycheck-next-error-function n reset))
+  (flycheck-next-error-function n reset)
+  (flycheck-display-error-at-point))
 
 (defun flycheck-previous-error (&optional n)
   "Visit the N-th previous error.
