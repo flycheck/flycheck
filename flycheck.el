flycheck/flycheck

commit 7a82b38c094a43eee2ab42608f4e3bd0bf61bf28

Merge message and checker columns in error list
This allows the message column to stretch arbitrarily far, without
adding meaningless ellipsis.

Fixes GH-828.

flycheck.el
@@ -3691,8 +3691,7 @@ the beginning of the buffer."
    ("Col" 3 nil :right-align t)
    ("Level" 8 flycheck-error-list-entry-level-<)
    ("ID" 6 t)
-   ("Message" 0 t)
-   (" (Checker)" 8 t)]
+   ("Message (Checker)" 0 t)]
   "Table format for the error list.")
 
 (define-derived-mode flycheck-error-list-mode tabulated-list-mode "Flycheck errors"
@@ -3748,9 +3747,15 @@ the beginning of the buffer."
   (flycheck-error-list-goto-error (button-start button)))
 
 (defsubst flycheck-error-list-make-cell (text &optional face)
-  "Make an error list cell with TEXT and FACE."
-  (let ((face (or face 'default)))
-    (list text 'type 'flycheck-error-list 'face face)))
+  "Make an error list cell with TEXT and FACE.
+
+If FACE is nil don't set a FACE on TEXT.  If TEXT already has
+face properties, do not specify a FACE.  Note though, that if
+TEXT gets truncated it will not inherit any previous face
+properties.  If you expect TEXT to be truncated in the error
+list, do specify a FACE explicitly!"
+  (append (list text 'type 'flycheck-error-list)
+          (and face (list 'face face))))
 
 (defsubst flycheck-error-list-make-number-cell (number face)
   "Make a table cell for a NUMBER with FACE.
@@ -3760,6 +3765,18 @@ string with attached text properties."
   (flycheck-error-list-make-cell
    (if (numberp number) (number-to-string number) "")
    face))
+
+(defun flycheck-error-list-make-last-column (message checker)
+  "Compute contents of the last error list cell.
+
+MESSAGE and CHECKER are displayed in a single column to allow the
+message to stretch arbitrarily far."
+  (let* ((checker-name (propertize (symbol-name checker)
+                                   'face 'flycheck-error-list-checker-name))
+         (text (format "%s (%s)" (flycheck-flush-multiline-message message)
+                       checker-name)))
+    (add-face-text-property 0 (length text) 'default t text)
+    text))
 
 (defun flycheck-error-list-make-entry (error)
   "Make a table cell for the given ERROR.
@@ -3784,10 +3801,7 @@ Return a list with the contents of the table cell."
                    (if id (format "%s" id) "")
                    'flycheck-error-list-id)
                   (flycheck-error-list-make-cell
-                   (flycheck-flush-multiline-message message))
-                  (flycheck-error-list-make-cell
-                   (format "(%s)" checker)
-                   'flycheck-error-list-checker-name)))))
+                   (flycheck-error-list-make-last-column message checker))))))
 
 (defun flycheck-compute-message-column-offset ()
   "Compute the amount of space to use in `flycheck-flush-multiline-message'."

test/specs/test-error-list.el
@@ -67,7 +67,7 @@
     (it "has a local header line"
       (flycheck/with-error-list-buffer
         (expect header-line-format
-                :to-equal " Line Col Level ID Message  (Checker) ")
+                :to-equal " Line Col Level ID Message (Checker) ")
         (expect 'header-line-format :to-be-local))))
 
   (describe "Columns"
@@ -95,12 +95,7 @@
     (it "has the error message in the 5th column"
       (flycheck/with-error-list-buffer
         (expect (aref tabulated-list-format 4)
-                :to-equal '("Message" 0 t))))
-
-    (it "has the syntax checker in the 6th column"
-      (flycheck/with-error-list-buffer
-        (expect (aref tabulated-list-format 5)
-                :to-equal '(" (Checker)" 8 t)))))
+                :to-equal '("Message (Checker)" 0 t)))))
 
   (describe "Entry"
     (let* ((warning (flycheck-error-new-at 10 12 'warning "A foo warning"
@@ -145,26 +140,22 @@
                       'type 'flycheck-error-list
                       'face 'flycheck-error-list-id)))
 
-      (it "has the error message in the 5th cell"
-        (expect (aref cells 4) :to-equal
-                (list "A foo warning"
-                      'type 'flycheck-error-list
-                      'face 'default)))
+      (let ((checker-name (propertize "emacs-lisp-checkdoc"
+                                      'face 'flycheck-error-list-checker-name)))
+        (it "has the error message in the 5th cell"
+          (let* ((message (format "A foo warning (%s)" checker-name)))
+            (add-face-text-property 0 (length message) 'default t message)
+            (expect (aref cells 4) :to-equal
+                    (list message 'type 'flycheck-error-list))))
 
-      (it "has a default message in the 5th cell if there is no message"
-        (cl-letf* (((flycheck-error-message warning) nil)
-                   (entry (flycheck-error-list-make-entry warning))
-                   (cells (cadr entry)))
-          (expect (aref cells 4) :to-equal
-                  (list "Unknown warning"
-                        'type 'flycheck-error-list
-                        'face 'default))))
-
-      (it "has the syntax checker in the 6th cell"
-        (expect (aref cells 5) :to-equal
-                (list "(emacs-lisp-checkdoc)"
-                      'type 'flycheck-error-list
-                      'face 'flycheck-error-list-checker-name)))))
+        (it "has a default message in the 5th cell if there is no message"
+          (cl-letf* (((flycheck-error-message warning) nil)
+                     (entry (flycheck-error-list-make-entry warning))
+                     (cells (cadr entry))
+                     (message (format "Unknown warning (%s)" checker-name)))
+            (add-face-text-property 0 (length message) 'default t message)
+            (expect (aref cells 4) :to-equal
+                    (list message 'type 'flycheck-error-list)))))))
 
   (describe "Filter"
     (it "kills the filter variable when resetting the filter"

commit 6ea2ffeddd328058d9c56e82e75bc3cf113c79f5

Fix alignment bug in flycheck-compute-message-column-offset
Introduced by 7a82b38c094a43eee2ab42608f4e3bd0bf61bf28.  The check used
to locate the message column was overly strict.

flycheck.el
@@ -3869,9 +3869,10 @@ Return a list with the contents of the table cell."
                                          (padding (plist-get props :pad-right)))
                               (cons name (+ width (or padding 1)))))
                           tabulated-list-format))
-         (before-msg (seq-take-while (lambda (fmt)
-                                       (not (string= (car fmt) "Message")))
-                                     widths)))
+         (before-msg (seq-take-while
+                      (lambda (fmt)
+                        (not (string-match-p "^Message" (car fmt))))
+                      widths)))
     (apply #'+ tabulated-list-padding (seq-map #'cdr before-msg))))
 
 (defun flycheck-flush-multiline-message (msg)