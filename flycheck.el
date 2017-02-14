24276fb3926d04700062c6c1e6b7f83b8bcfd83a
fmdkdd <fmdkdd@gmail.com>
2017-02-14 18:39:42 +0100

    Fix calling display-error twice after next-error

--- flycheck.el
+++ flycheck.el
@@ -4391,6 +4391,8 @@ Intended for use with `next-error-function'."
       (flycheck-jump-to-error err)
     (user-error "No more Flycheck errors")))
 
+(defvar-local flycheck--display-error-caused-by-next-error nil)
+
 (defun flycheck-next-error (&optional n reset)
   "Visit the N-th error from the current point.
 
@@ -4403,7 +4405,8 @@ position."
     ;; Universal prefix argument means reset
     (setq reset t n nil))
   (flycheck-next-error-function n reset)
-  (flycheck-display-error-at-point))
+  (flycheck-display-error-at-point)
+  (setq flycheck--display-error-caused-by-next-error t))
 
 (defun flycheck-previous-error (&optional n)
   "Visit the N-th previous error.
@@ -4946,10 +4949,13 @@ non-nil."
 (defun flycheck-display-error-at-point-soon ()
   "Display the first error message at point in minibuffer delayed."
   (flycheck-cancel-error-display-error-at-point-timer)
-  (when (flycheck-overlays-at (point))
+  (when (and (flycheck-overlays-at (point))
+             (not flycheck--display-error-caused-by-next-error))
     (setq flycheck-display-error-at-point-timer
           (run-at-time flycheck-display-errors-delay nil
-                       'flycheck-display-error-at-point))))
+                       'flycheck-display-error-at-point)))
+  (setq flycheck--display-error-caused-by-next-error nil))
+
 
 
 ;;; Functions to display errors
