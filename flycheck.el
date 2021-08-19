75ad52ddca53aacfefafc8167898ee67c24004b4
Billy.Zheng <vil963@gmail.com>
2021-08-19 17:15:00 +0800

    Fix Eruby checker not works on web-mode.

--- flycheck.el
+++ flycheck.el
@@ -8919,7 +8919,7 @@ See URL `http://www.kuwata-lab.com/erubis/'."
   :command ("erubis" "-z" source)
   :error-patterns
   ((error line-start (file-name) ":" line ": " (message) line-end))
-  :modes (html-erb-mode rhtml-mode)
+  :modes (html-erb-mode rhtml-mode web-mode)
   :next-checkers ((warning . eruby-ruumba)))
 
 (flycheck-def-config-file-var flycheck-ruumbarc eruby-ruumba ".ruumba.yml")
@@ -8960,7 +8960,7 @@ See URL `https://github.com/ericqweinstein/ruumba'."
    (error line-start (file-name) ":" line ":" column ": " (or "E" "F") ": "
           (optional (id (one-or-more (not (any ":")))) ": ") (message)
           line-end))
-  :modes (html-erb-mode rhtml-mode))
+  :modes (html-erb-mode rhtml-mode web-mode))
 
 (flycheck-def-args-var flycheck-gfortran-args fortran-gfortran
   :package-version '(flycheck . "0.22"))
