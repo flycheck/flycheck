a4d782e7af12e20037c0cecf0d4386cd2676c085
Bozhidar Batsov <bozhidar@batsov.dev>
2025-05-27 12:07:54 +0300

    Add a comment about text -> concise in ruff

--- flycheck.el
+++ flycheck.el
@@ -10892,6 +10892,7 @@ See URL `https://docs.astral.sh/ruff/'."
   :command ("ruff"
             "check"
             (config-file "--config" flycheck-python-ruff-config)
+            ;; older versions of ruff (before 0.2) used "text" instead of "concise"
             "--output-format=concise"
             (option "--stdin-filename" buffer-file-name)
             "-")
