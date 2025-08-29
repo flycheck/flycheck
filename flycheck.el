a5bf87a910c1591123cfa28bd57d9be90c970123
Mario Rodas <marsam@users.noreply.github.com>
2025-08-29 04:20:00 +0000

    Add tests for python-ruff checker

--- flycheck.el
+++ flycheck.el
@@ -10909,7 +10909,7 @@ See URL `https://docs.astral.sh/ruff/'."
   :error-patterns
   ((error line-start
           (or "-" (file-name)) ":" line ":" (optional column ":") " "
-          "SyntaxError: "
+          (or "SyntaxError: " "invalid-syntax: ")
           (message (one-or-more not-newline))
           line-end)
    (warning line-start
