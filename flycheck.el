a882a4033fc530729fcbf32bcd96edd2da089b21
Peter Oliver <git@mavit.org.uk>
2026-07-31 16:39:00 +0100

    perlimports: Include the trailing semicolon in the erroring region

--- flycheck.el
+++ flycheck.el
@@ -14915,7 +14915,7 @@ for more information about perlimports."
                          (font-lock-ensure)
                          (buffer-string)))
                :end-line .location.end.line
-               :end-column .location.end.column
+               :end-column (+ 1 .location.end.column)
                :checker checker
                :buffer buffer)))
           (flycheck-parse-json output)))
